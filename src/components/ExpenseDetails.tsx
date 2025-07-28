5d59801921accf4c5f7a1b55c02dc3346f95f4de
kevinocotan <kaocotan@gmail.com>
2025-07-27 23:04:36 -0600

    Creando el Componente para mostrar los gastos

---
+++ src/components/ExpenseDetails.tsx
@@ -0,0 +1,11 @@
+import { Expense } from "../types"
+
+type ExpenseDetailsProps = {
+    expense : Expense
+}
+
+export default function ExpenseDetails({expense }: ExpenseDetailsProps) {
+  return (
+    <div>ExpenseDetails</div>
+  )
+}
